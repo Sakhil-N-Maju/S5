8a20f6890901e188cec549f9897e6b624028edde
Sakhil-N-Maju <sakhil.n.maju123@gmail.com>
2025-07-07 11:07:53 +0530

    Create 4.py

---
+++ AI ALGORITHMS LAB/EXP 1/4/4.py
@@ -0,0 +1,14 @@
+is_sunny = True
+have_umbrella = False
+
+print("Should you go outside ? ")
+if is_sunny == True or have_umbrella == True:
+	print("can go outside")
+else:
+	print("dont go outside")
+	
+print("Should you carry the umbrella ? ")	
+if is_sunny == False:
+	print("you should carry an ubrella")
+else:
+	print("you dont have to carry an umbrella")		
